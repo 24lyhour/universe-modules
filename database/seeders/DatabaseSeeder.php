20b4ba60528ed0e4555ead40396b80417e62f0ae
Lyhour <kouchlyhour@gmail.com>
2026-02-16 15:43:59 +0700

    feat: Integrate module-specific seeders into the main database seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Core seeders
         $this->call([
             UserSeeder::class,
             SettingsSeeder::class,
+        ]);
+
+        // Module seeders in dependency order
+        $this->call([
+            // Customer module (needed for Wallets)
+            \Modules\Customer\Database\Seeders\CustomerDatabaseSeeder::class,
+
+            // Outlet module (needed for Menu)
+            \Modules\Outlet\Database\Seeders\OutletDatabaseSeeder::class,
+
+            // Menu module (depends on Outlet)
+            \Modules\Menu\Database\Seeders\MenuDatabaseSeeder::class,
+
+            // Wallets module (depends on Customer)
+            \Modules\Wallets\Database\Seeders\WalletsDatabaseSeeder::class,
         ]);
     }
 }
