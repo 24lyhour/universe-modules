7a84532d6174934e259974f3c8d3e20b0f371757
Lyhour <kouchlyhour@gmail.com>
2026-02-16 15:48:09 +0700

    feat: Integrate module-specific seeders into the main database seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,19 +19,19 @@ class DatabaseSeeder extends Seeder
             SettingsSeeder::class,
         ]);
 
-        // Module seeders in dependency order
-        $this->call([
-            // Customer module (needed for Wallets)
-            \Modules\Customer\Database\Seeders\CustomerDatabaseSeeder::class,
+        // // Module seeders in dependency order
+        // $this->call([
+        //     // Customer module (needed for Wallets)
+        //     \Modules\Customer\Database\Seeders\CustomerDatabaseSeeder::class,
 
-            // Outlet module (needed for Menu)
-            \Modules\Outlet\Database\Seeders\OutletDatabaseSeeder::class,
+        //     // Outlet module (needed for Menu)
+        //     \Modules\Outlet\Database\Seeders\OutletDatabaseSeeder::class,
 
-            // Menu module (depends on Outlet)
-            \Modules\Menu\Database\Seeders\MenuDatabaseSeeder::class,
+        //     // Menu module (depends on Outlet)
+        //     \Modules\Menu\Database\Seeders\MenuDatabaseSeeder::class,
 
-            // Wallets module (depends on Customer)
-            \Modules\Wallets\Database\Seeders\WalletsDatabaseSeeder::class,
-        ]);
+        //     // Wallets module (depends on Customer)
+        //     \Modules\Wallets\Database\Seeders\WalletsDatabaseSeeder::class,
+        // ]);
     }
 }
