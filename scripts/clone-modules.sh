315911b2a349aa33907d1a35bcaa974ec9f043f7
Lyhour <kouchlyhour@gmail.com>
2026-02-23 11:54:27 +0700

    feat: Add support for cloning private GitHub modules using GITHUB_TOKEN.

--- scripts/clone-modules.sh
+++ scripts/clone-modules.sh
@@ -1,38 +1,56 @@
 #!/bin/bash
 # Clone module submodules manually if they're empty
+# Uses GITHUB_TOKEN env var for private repo access
 
 MODULES_DIR="Modules"
 
+# Module repos
 declare -A MODULES=(
-    ["Blog"]="https://github.com/24lyhour/universe-modules-Blog.git"
-    ["Booking"]="https://github.com/24lyhour/universe-modules-Booking.git"
-    ["Company"]="https://github.com/24lyhour/universe-modules-Company.git"
-    ["Customer"]="https://github.com/24lyhour/universe-modules-Customer.git"
-    ["Employee"]="https://github.com/24lyhour/universe-modules-Employee.git"
-    ["Hotel"]="https://github.com/24lyhour/universe-modules-Hotel.git"
-    ["Media"]="https://github.com/24lyhour/universe-modules-Media.git"
-    ["Menu"]="https://github.com/24lyhour/universe-modules-Menu.git"
-    ["Movice"]="https://github.com/24lyhour/universe-modules-Movice.git"
-    ["Order"]="https://github.com/24lyhour/universe-modules-Order.git"
-    ["Outlet"]="https://github.com/24lyhour/universe-modules-Outlet.git"
-    ["Payment"]="https://github.com/24lyhour/universe-modules-Payment.git"
-    ["Porfolio"]="https://github.com/24lyhour/universe-modules-Porfolio.git"
-    ["Product"]="https://github.com/24lyhour/universe-modules-Product.git"
-    ["Report"]="https://github.com/24lyhour/universe-modules-Report.git"
-    ["School"]="https://github.com/24lyhour/universe-modules-School.git"
-    ["Wallets"]="https://github.com/24lyhour/universe-modules-Wallet.git"
+    ["Blog"]="universe-modules-Blog"
+    ["Booking"]="universe-modules-Booking"
+    ["Company"]="universe-modules-Company"
+    ["Customer"]="universe-modules-Customer"
+    ["Employee"]="universe-modules-Employee"
+    ["Hotel"]="universe-modules-Hotel"
+    ["Media"]="universe-modules-Media"
+    ["Menu"]="universe-modules-Menu"
+    ["Movice"]="universe-modules-Movice"
+    ["Order"]="universe-modules-Order"
+    ["Outlet"]="universe-modules-Outlet"
+    ["Payment"]="universe-modules-Payment"
+    ["Porfolio"]="universe-modules-Porfolio"
+    ["Product"]="universe-modules-Product"
+    ["Report"]="universe-modules-Report"
+    ["School"]="universe-modules-School"
+    ["Wallets"]="universe-modules-Wallet"
 )
 
+# Build clone URL with token if available
+get_clone_url() {
+    local repo_name=$1
+    if [ -n "$GITHUB_TOKEN" ]; then
+        echo "https://${GITHUB_TOKEN}@github.com/24lyhour/${repo_name}.git"
+    else
+        echo "https://github.com/24lyhour/${repo_name}.git"
+    fi
+}
+
 echo "=== Checking and cloning modules ==="
+if [ -n "$GITHUB_TOKEN" ]; then
+    echo "Using GITHUB_TOKEN for private repo access"
+else
+    echo "WARNING: No GITHUB_TOKEN set - private repos will fail"
+fi
 
 for module in "${!MODULES[@]}"; do
     MODULE_PATH="$MODULES_DIR/$module"
+    CLONE_URL=$(get_clone_url "${MODULES[$module]}")
 
     # Check if module directory exists and has content
     if [ ! -d "$MODULE_PATH" ] || [ -z "$(ls -A $MODULE_PATH 2>/dev/null)" ]; then
         echo "Cloning $module..."
         rm -rf "$MODULE_PATH"
-        git clone --depth 1 "${MODULES[$module]}" "$MODULE_PATH"
+        git clone --depth 1 "$CLONE_URL" "$MODULE_PATH"
     else
         echo "$module already exists"
     fi
