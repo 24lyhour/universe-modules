ef338bf718599290a28ca2d18751c7377ef2dbea
Lyhour <kouchlyhour@gmail.com>
2026-03-18 19:37:15 +0700

    new

--- resources/js/routes/product/index.ts
+++
@@ -1,9 +0,0 @@
-import products from './products'
-import productTypes from './product-types'
-
-const product = {
-    products: Object.assign(products, products),
-    productTypes: Object.assign(productTypes, productTypes),
-}
-
-export default product
