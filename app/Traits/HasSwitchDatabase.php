c72a2705bbf714fba771993d705194e1c3707eea
Lyhour <kouchlyhour@gmail.com>
2026-03-29 21:42:19 +0700

    feat: add docblock to getConnectionName in HasSwitchDatabase trait

--- app/Traits/HasSwitchDatabase.php
+++ app/Traits/HasSwitchDatabase.php
@@ -4,8 +4,19 @@ namespace App\Traits;
 
 trait HasSwitchDatabase
 {
+    /**
+     * database name for the configure
+     */
     public function getConnectionName()
     {
         return config('database.order_connection', 'mysql');
+    }
+
+    /**
+     * switch database name for the configure
+     */
+    public function switchDatabase($database)
+    {
+        config(['database.order_connection' => $database]);
     }
 }
