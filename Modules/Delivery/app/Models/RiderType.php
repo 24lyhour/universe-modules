98dd1e8197ce439c3caba90c2daf162f90b018d6
Lyhour <kouchlyhour@gmail.com>
2026-04-24 22:56:02 +0700

    feat: initialize Landing module and update Delivery rider models

--- Modules/Delivery/app/Models/RiderType.php
+++ Modules/Delivery/app/Models/RiderType.php
@@ -13,7 +13,37 @@ class RiderType extends Model
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [];
+    protected $fillable = [
+        'id',
+        'uuid',
+        'rider_id',
+        'vehicle_name',
+        'vehicle_plate',
+        'note',
+        'is_active',
+        'created_at',
+        'updated_at',
+
+    ];
+
+
+    /**
+     * Protect the filed
+     */
+    protected $case = [
+        'is_active' =>'boolean',
+        'uuid'      => 'string',
+        'create_at' => 'datetime',
+        'update_at' => 'datetime',
+
+    ];
+
+    /**
+     * gloabl scope 
+     */
+    public function scopeActive($query) {
+        return $query->where('is_active', 1);
+    }  
 
 
     /**
@@ -22,5 +52,13 @@ class RiderType extends Model
     protected static function newFactory(): RiderTypeFactory
     {
         return RiderTypeFactory::new();
+    }
+
+    /**
+     * relation to 
+     */
+    public function rider() : BelongsTo
+    {
+        return $this->belongsTo(Rider::class);
     }
 }
