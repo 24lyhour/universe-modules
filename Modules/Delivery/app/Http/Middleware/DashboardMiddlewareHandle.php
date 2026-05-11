f765590b5d95d3eea2b8b9bd681e6459e45c6746
Lyhour <kouchlyhour@gmail.com>
2026-05-11 09:05:27 +0700

    refactor: unify DashboardMiddlewareHandle logic across Delivery and Landing modules

---
+++ Modules/Delivery/app/Http/Middleware/DashboardMiddlewareHandle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Delivery\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class DashboardMiddlewareHandle
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+}
